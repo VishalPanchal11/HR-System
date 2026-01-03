ba138228a3e657b45bfd51847b4bd7f39490a8dc
SakshiBunge <bungesakshi@.com>
2026-01-03 11:41:15 +0530

    Fixed resignation module

---
+++ Admin/Resignation.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Masters/Admin.Master" AutoEventWireup="true" CodeBehind="Resignation.aspx.cs" Inherits="HR_System.Admin.WebForm1" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link href="~/Content/resignation.css" rel="stylesheet" />
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <div class="card">
+    <h2>Employee Resignation</h2>
+
+    <asp:HiddenField ID="hfResignationID" runat="server" />
+
+    <label>User ID</label>
+    <asp:TextBox ID="txtUserID" runat="server" CssClass="input" />
+
+    <label>Department ID</label>
+    <asp:TextBox ID="txtDeptID" runat="server" CssClass="input" />
+
+    <label>Notice Date</label>
+    <asp:TextBox ID="txtNoticeDate" runat="server" TextMode="Date" CssClass="input" />
+
+    <label>Resign Date</label>
+    <asp:TextBox ID="txtResignDate" runat="server" TextMode="Date" CssClass="input" />
+
+    <label>Reason</label>
+    <asp:TextBox ID="txtReason" runat="server" TextMode="MultiLine" CssClass="textarea" />
+
+    <asp:Button ID="btnSave" runat="server" Text="Save" CssClass="btn"
+        OnClick="btnSave_Click" />
+</div>
+
+<hr />
+
+<asp:GridView ID="gvResignation" runat="server" AutoGenerateColumns="false"
+    CssClass="grid" OnRowCommand="gvResignation_RowCommand">
+    <Columns>
+        <asp:BoundField DataField="ResignationID" HeaderText="ID" />
+        <asp:BoundField DataField="UserID" HeaderText="User" />
+        <asp:BoundField DataField="DepartmentId" HeaderText="Department" />
+        <asp:BoundField DataField="NoticeDate" HeaderText="Notice Date" />
+        <asp:BoundField DataField="ResignDate" HeaderText="Resign Date" />
+        <asp:BoundField DataField="Reason" HeaderText="Reason" />
+
+        <asp:TemplateField HeaderText="Action">
+            <ItemTemplate>
+                <asp:Button ID="btnDelete"
+                    runat="server"
+                    Text="Delete"
+                    CssClass="btn"
+                    CommandName="del"
+                    CommandArgument='<%# Eval("ResignationID") %>'
+                    OnClientClick="return confirm('Are you sure you want to delete?');" />
+            </ItemTemplate>
+        </asp:TemplateField>
+
+    </Columns>
+</asp:GridView>
+</asp:Content>
---
+++ Admin/Resignation.aspx.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HR_System.Admin
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string conStr = ConfigurationManager.ConnectionStrings["HRdbCon"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // ADMIN ONLY ACCESS
+            if (Session["Role"] == null || Session["Role"].ToString() != "Admin")
+            {
+                Response.Redirect("~/Login.aspx");
+            }
+
+            if (!IsPostBack)
+            {
+                LoadGrid();
+            }
+        }
+
+        void LoadGrid()
+        {
+            using (SqlConnection con = new SqlConnection(conStr))
+            {
+                SqlDataAdapter da = new SqlDataAdapter("sp_GetResignations", con);
+                da.SelectCommand.CommandType = CommandType.StoredProcedure;
+
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                gvResignation.DataSource = dt;
+                gvResignation.DataBind();
+            }
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(conStr))
+            {
+                SqlCommand cmd = new SqlCommand("sp_InsertResignation", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@UserID", txtUserID.Text);
+                cmd.Parameters.AddWithValue("@DepartmentId", txtDeptID.Text);
+                cmd.Parameters.AddWithValue("@NoticeDate", txtNoticeDate.Text);
+                cmd.Parameters.AddWithValue("@ResignDate", txtResignDate.Text);
+                cmd.Parameters.AddWithValue("@Reason", txtReason.Text);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+
+            LoadGrid();
+            Clear();
+        }
+
+        protected void gvResignation_RowCommand(object sender, System.Web.UI.WebControls.GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "del")
+            {
+                using (SqlConnection con = new SqlConnection(conStr))
+                {
+                    SqlCommand cmd = new SqlCommand("sp_DeleteResignation", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@ResignationID", e.CommandArgument);
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                }
+                LoadGrid();
+            }
+        }
+
+        void Clear()
+        {
+            txtUserID.Text = "";
+            txtDeptID.Text = "";
+            txtNoticeDate.Text = "";
+            txtResignDate.Text = "";
+            txtReason.Text = "";
+        }
+    }
+}
