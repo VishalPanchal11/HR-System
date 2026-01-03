04fcf0cd5e1fca090a5fe8a94ac3b63f84c81a0d
siddsak <siddsakpal2004@gmail.com>
2026-01-03 23:37:44 +0530

    attendance module created for admin

---
+++ Admin/Attendance/AddLeave.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HR_System.Admin.Attendance
+{
+    public partial class AddLeave : System.Web.UI.Page
+    {
+        SqlConnection conn;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string str = ConfigurationManager.ConnectionStrings["HRdbCon"].ConnectionString;
+            conn = new SqlConnection(str);
+            conn.Open();
+            if (!IsPostBack)
+            {
+                DropDownn();
+            }
+        }
+        public void DropDownn()
+        {
+            using (SqlCommand cmd = new SqlCommand("SELECT DepartmentId,Name FROM Departments WHERE Status ='Active'", conn))
+            { 
+                cmd.CommandType = CommandType.Text;
+                using (SqlDataReader rdr = cmd.ExecuteReader())
+                {
+                    ddlDepartment.DataSource = rdr;
+                    ddlDepartment.DataTextField = "Name";
+                    ddlDepartment.DataValueField = "DepartmentId";
+                    ddlDepartment.DataBind();
+                }
+            }
+            using (SqlCommand cmd1= new SqlCommand("SELECT LeaveTypeId,LeaveType from MasterLeaveTypes", conn))
+            {
+                cmd1.CommandType = CommandType.Text;
+                using (SqlDataReader rd= cmd1.ExecuteReader())
+                {
+                    ddlLeaveType.DataSource = rd;
+                    ddlLeaveType.DataTextField = "LeaveType";
+                    ddlLeaveType.DataValueField = "LeaveTypeId";
+                    ddlLeaveType.DataBind();
+                }
+            }
+
+        }
+        protected void btnAllocate_Click(object sender, EventArgs e)
+        {
+            string departmentname = ddlDepartment.SelectedValue;
+            string leaveType = ddlLeaveType.SelectedValue;
+            string Leaves = txtLeaves.Text;
+            string status = ddlStatus.SelectedValue;
+            SqlCommand cmd = new SqlCommand("Pro_AddLeave_Dept", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@DepartmentId", ddlDepartment.SelectedValue);
+            cmd.Parameters.AddWithValue("@LeaveTypeId", ddlLeaveType.SelectedValue);
+            cmd.Parameters.AddWithValue("@LeavesCount",txtLeaves.Text);
+            cmd.Parameters.AddWithValue("@Status", ddlStatus.SelectedValue);
+            cmd.ExecuteNonQuery();
+   
+        }
+    }
+}
---
+++ Admin/Attendance/Attendance.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HR_System.Admin.Attendance
+{
+    public partial class Attendance : System.Web.UI.Page
+    {
+        SqlConnection conn;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+       
+                string str = ConfigurationManager.ConnectionStrings["HRdbCon"].ConnectionString;
+                conn = new SqlConnection(str);
+                conn.Open();
+        }
+
+        protected void SubmitAddType(object sender, EventArgs e)
+        {
+     
+            string leavetype = LeaveType.Text;
+            SqlCommand cmd = new SqlCommand("Pro_InsertMasterLeaveTypes", conn);
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@LeaveType", leavetype);
+            cmd.ExecuteNonQuery();
+        }
+
+        protected void gvLeaveTypes_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            
+            int id = Convert.ToInt32(gvLeaveTypes.DataKeys[e.RowIndex].Value);
+            SqlCommand cmd = new SqlCommand("Pro_DeleteMasterLeaveTypes", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@LeaveTypeId", id); 
+            cmd.ExecuteNonQuery();
+           
+        }
+    }
+}   
