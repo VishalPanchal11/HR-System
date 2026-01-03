4d195c95f97c80a9f4dfe44fbf97b2d66ca3d2d3
Dheeraj8850 <dheerajgupta88502857@gmail.com>
2026-01-03 12:52:45 +0530

    Sql Event Procedure

--- HRSystemSQL.sql
+++ HRSystemSQL.sql
@@ -1,3 +1,5 @@
+
+
 select *  from [User];
 -- user procs
 -- user triggers
@@ -8,5 +10,90 @@ select * from AdminDocuments;
 select * from addAdminDocNames;
 select * from addEmployeeDocNames;
 
+--Dheeraj Module
+--Admin 
+<<<<<<< HEAD
 select * from Events
 select * from EventTypes
+=======
+
+create procedure Pro_EventType
+@Name nvarchar(50),
+@Color nvarchar(20) 
+as
+begin
+insert into EventTypes values(@Name, @Color)
+end;
+
+--Show 
+create procedure show_EventType
+as
+begin
+select * from EventTypes
+end;
+
+--delete
+create procedure delete_EventType
+@id int
+as
+begin
+delete from EventTypes where id =@id
+end;
+
+-- Event insert
+create procedure insert_Event
+@title nvarchar(max),
+@Date nvarchar(max),
+@EventTypeId int
+as
+begin
+insert into Events (title, [Date], EventTypeId) values(@title, @Date, @EventTypeId)
+end
+
+-- event update
+create procedure update_Event
+@id int,
+@title nvarchar(max),
+@Date nvarchar(max),
+@EventTypeId int
+as
+begin
+update Events set Title=@title, [Date]= @Date, EventTypeId=@EventTypeId where id=@id
+end;
+
+-- event delete 
+create procedure delete_Event
+@id int
+as
+begin
+delete from Events where id = @id
+end
+
+
+
+--Siddhesh Module
+--Admin
+--1) Adding leave type
+select*from MasterLeaveTypes;
+
+--2)Attdendance
+select*from Attendance;
+select*from Deduction;
+select*from Earning;
+select*from EarningType;
+select*from EmployeeDeductions;
+select*from DeductionType;
+select*from EmployeeEarnings;
+select*from EmployeeSalaries;
+select*from LeaveBalances;
+select*from LeaveRequests;
+
+select*from Payslips;
+select*from Timesheets;
+select*from [User];
+
+
+
+
+
+>>>>>>> 90c677be47ae38f2e575c343a842c4eb354b3ae9
